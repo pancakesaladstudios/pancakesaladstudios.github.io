e635b6a312a9c106e4efcabcafb3ba521f8ae3d6
Josh Horsley <jhorsley@MacBook-Pro.local>
2020-07-17 00:17:38 -0500

    adding site files

---
+++ _next/static/TYJq-HEzgvxBDTiFPBNsW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/games":[a],"/index":[a]}}("static\u002Fchunks\u002Fa73f63d24c6193391222e4cf7486e65889238ea0.f086d111217f84fd20cc.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
